36da4b6ca92f53e3c83e95e10e219d29fb91b54e
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-06-28 14:06:54 -0500

    Implemented API for fetching Urls

--- resources/db.js
+++ resources/db.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
-// let uri = 'mongodb+srv://naveena:naveena@cluster0-6rknx.mongodb.net/viprahub?retryWrites=true';
+let uri = 'mongodb+srv://naveena:naveena@cluster0-6rknx.mongodb.net/viprahub?retryWrites=true';
 
-let uri = 'mongodb://localhost:27017/viprahub';
+// let uri = 'mongodb://localhost:27017/viprahub';
 mongoose.connect(uri, { useNewUrlParser: true });
 
 mongoose.connection.on("connected", () => {
