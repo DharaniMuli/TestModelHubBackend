f2e0e39698430dbb92b241557532a33d9329bbc0
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-07-09 16:06:42 -0500

    mongo conn

--- resources/db.js
+++ resources/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-let uri = 'mongodb+srv://naveena:naveena@cluster0-6rknx.mongodb.net/modelhub?retryWrites=true';
+let uri = 'mongodb+srv://naveena:naveena@cluster0-6rknx.mongodb.net/viprahub?retryWrites=true';
 
  // let uri= 'mongodb+srv://Dharani:OmSaiRam@246@cluster0-yfvvs.mongodb.net/modelhub?retryWrites=true&w=majority';
 
